f16354ed983657cef9a5bf293043914167fe03fa
Jordan Race <diamondhojo@gmail.com>
2021-09-04 18:28:54 -0400

    Update First Half.py

--- First Half.py
+++ First Half.py
@@ -2,17 +2,16 @@
 First Half
 https://codingbat.com/prob/p107010
 
-Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+Give a string of even length, this returns the first half. So the string "WooHoo" yields "Woo".
 
 first_half('WooHoo') → 'Woo'
-first_half('HelloThere') → 'Hello'
-first_half('abcdef') → 'abc'
 """
+
 def first_half(string):
-    if (int(len(string).real/2) % 2) == 0:
+    if (int(len(string)) % 2) == 0:
         leftHalf = len(string).real/2
         print(string[:int(leftHalf)]) #Pseudo Code --> print(string[:2])
     else:
-        print("Please enter an even number")
+        first_half(input("Enter a single word w/ even number of characters: "))
 
-first_half(input("Enter a single word: "))
+first_half(input("Enter a single word w/ even number of characters: "))
